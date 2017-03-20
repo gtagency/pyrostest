3249831545d6e709574b55c0e7b5145130e90b35
Joshua Morton <joshua.morton13@gmail.com>
2017-03-20 02:20:05 -0400

    Fix __init__.py imports for new package.

--- pyrostest/__init__.py
+++ pyrostest/__init__.py
@@ -1,6 +1,6 @@
 """Collection of utilities for testing ros nodes less painfully.
 """
 
-from tests.test_utils.rostest_utils import RosTest
-from tests.test_utils.launch_tools import with_launch_file, launch_node
-from tests.test_utils.mock_nodes import mock_pub, check_topic
+from pyrostest.rostest_utils import RosTest
+from pyrostest.launch_tools import with_launch_file, launch_node
+from pyrostest.mock_nodes import mock_pub, check_topic
