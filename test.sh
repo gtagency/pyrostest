8dd7a674acdfa9b90a410d11c2d93e9f15fe2ae6
Joshua Morton <joshua.morton13@gmail.com>
2017-04-15 19:24:59 -0400

    Added rosdep creation.

--- test.sh
+++ test.sh
@@ -5,6 +5,8 @@ set -e
 source /opt/ros/indigo/setup.sh
 cd ../catkin_ws
 catkin_init_workspace
+sudo rosdep init
+rosdep update
 rosdep install -y --from-paths ./pyrostest --ignore-src --rosdistro=indigo
 catkin_make
 source devel/setup.sh
