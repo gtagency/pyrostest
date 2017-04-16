48b31adab66f3e3298a8033d165422ec21fd9e01
Joshua Morton <joshua.morton13@gmail.com>
2017-04-16 16:13:09 -0400

    Fix inconsistency between package names.

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ pip install catkin-pkg
 catkin_init_workspace
 sudo rosdep init
 rosdep update
-rosdep install -y --from-paths ./pyrostester --ignore-src --rosdistro=indigo
+rosdep install -y --from-paths ./pyrostest --ignore-src --rosdistro=indigo
 cd ..
 catkin_make
 source devel/setup.sh
