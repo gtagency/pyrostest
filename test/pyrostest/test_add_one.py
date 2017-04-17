2e0e46ac3e9693b63708a76eab4e999454e3ff26
Joshua Morton <joshua.morton13@gmail.com>
2017-04-16 23:22:20 -0400

    Namespaced topics instead of absolute.

--- test/pyrostest/test_add_one.py
+++ test/pyrostest/test_add_one.py
@@ -7,8 +7,8 @@ import pyrostest
 class TestAddOne(pyrostest.RosTest):
     @pyrostest.launch_node('pyrostest', 'add_one.py')
     def test_add_one_node(self):
-        with pyrostest.mock_pub('/subber', Int64, queue_size=0) as sub:
-            with pyrostest.check_topic('/add_one', Int64) as out:
+        with pyrostest.mock_pub('subber', Int64, queue_size=0) as sub:
+            with pyrostest.check_topic('add_one', Int64) as out:
                 sub.send(Int64(5))
                 assert out.message.data == 6
 
