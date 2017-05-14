f14300a9c624b2ee0c53081f8e4e5293bf1c0aec
Joshua Morton <joshua.morton13@gmail.com>
2017-05-14 17:49:39 -0400

    Renaming pass.
    Cleans up the API significantly on the road to 1.0.

--- pyrostest/__init__.py
+++ pyrostest/__init__.py
@@ -1,6 +1,5 @@
 """Collection of utilities for testing ros nodes less painfully.
 """
 
-from pyrostest.rostest_utils import RosTest
 from pyrostest.launch_tools import with_launch_file, launch_node
-from pyrostest.mock_nodes import mock_pub, check_topic
+from pyrostest.pyrostest import RosTest
