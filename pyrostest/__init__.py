bea3664f513c5b1d4364e1ca20bf2613ddcb04b6
Joshua Morton <joshua.morton13@gmail.com>
2017-05-14 18:17:49 -0400

    More renaming to fix test failures.

--- pyrostest/__init__.py
+++ pyrostest/__init__.py
@@ -1,5 +1,8 @@
 """Collection of utilities for testing ros nodes less painfully.
 """
 
+import pyrostest.rostest_utils
+
 from pyrostest.launch_tools import with_launch_file, launch_node
-from pyrostest.pyrostest import RosTest
+from pyrostest.ros_test import RosTest
+
