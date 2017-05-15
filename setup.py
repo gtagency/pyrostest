ebd972306d1002a32e95c5d625904eb20a82da3f
Joshua Morton <joshua.morton13@gmail.com>
2017-05-15 02:34:43 -0400

    Minor version bump because of major bugfix.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test = [
 
 setup(
     name='pyrostest',
-    version='0.0.5',
+    version='0.1.0',
     description='The most lit ros testing framework',
     long_description=long_description,
     packages=['pyrostest'],
