aa6df5b1ca4801cdaa85f7546c292be4f34e0107
Joshua Morton <joshua.morton13@gmail.com>
2017-05-22 02:21:40 -0400

    Rename tests so that they run.

--- test/pyrostest/test_system.py
+++ test/pyrostest/test_system.py
@@ -3,33 +3,33 @@ import pytest
 import pyrostest
 
 class TestSpinUp(pyrostest.RosTest):
-    def noop(self):
+    def test_noop(self):
         pass
 
     @pyrostest.launch_node('pyrostest', 'add_one.py')
-    def launches_node(self):
+    def test_launches_node(self):
         pass
 
-class FailureCases(pyrostest.RosTest):
+class TestFailureCases(pyrostest.RosTest):
     @pytest.mark.xfail(strict=True)
     @pyrostest.launch_node('this_isnt_a_project', 'add_one.py')
-    def no_rospackage(self):
+    def test_no_rospackage(self):
         pass
 
 
     @pytest.mark.xfail(strict=True)
     @pyrostest.launch_node('pyrostest', 'this_isnt_a_rosnode.py')
-    def no_node(self):
+    def test_no_node(self):
         pass
 
     
     @pytest.mark.xfail(strict=True)
     @pyrostest.with_launch_file('pyrostest', 'does_not_exist')
-    def no_launch_file(self):
+    def test_no_launch_file(self):
         pass
 
     @pytest.mark.xfail(strict=True)
     @pyrostest.with_launch_file('not_a_package', 'exists')
-    def no_launch_package(self):
+    def test_no_launch_package(self):
         pass
 
