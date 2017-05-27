a50d296e6661d4526fe00e1970a3522112b1934f
Joshua Morton <joshua.morton13@gmail.com>
2017-05-27 18:47:30 -0400

    Added a launch file and accompanying tests.

--- test/pyrostest/test_system.py
+++ test/pyrostest/test_system.py
@@ -10,6 +10,22 @@ class TestSpinUp(pyrostest.RosTest):
     def test_launches_node(self):
         pass
 
+    @pyrostest.with_launch_file('pyrostest', 'launch.launch')
+    def test_just_launch(self):
+        pass
+
+
+    @pyrostest.with_launch_file('pyrostest', 'launch.launch')
+    @pyrostest.launch_node('pyrostest', 'add_one.py')
+    def test_launch_and_unused_node(self):
+        pass
+
+    @pyrostest.with_launch_file('pyrostest', 'launch.launch')
+    @pyrostest.launch_node('pyrostest', 'pub_foo.py')
+    def test_launch_and_used_node(self):
+        pass
+
+
 class TestFailureCases(pyrostest.RosTest):
     @pytest.mark.xfail(strict=True)
     @pyrostest.launch_node('this_isnt_a_project', 'add_one.py')
