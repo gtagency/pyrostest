cf52a25001a3c9883caff70d3eb0318c8418fa76
Joshua Morton <joshua.morton13@gmail.com>
2017-05-27 20:13:07 -0400

    Use absolute path to make the node work.

--- test/pyrostest/pub_foo.py
+++ test/pyrostest/pub_foo.py
@@ -9,7 +9,7 @@ pub = rospy.Publisher('/pub_val', Int32, queue_size=10)
 def pub_foo():
     rospy.init_node('pub_foo', anonymous=True)
     while True:
-        pub.publish(rospy.get_param('foo'))
+        pub.publish(rospy.get_param('/foo'))
 
 
 if __name__ == '__main__':
