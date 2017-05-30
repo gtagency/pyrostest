c84b5a4204cea86c59c501e8191a8f7e4aa30a11
Joshua Morton <joshua.morton13@gmail.com>
2017-05-29 21:05:42 -0400

    Updated version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test = [
 
 setup(
     name='pyrostest',
-    version='0.1.3',
+    version='0.1.4',
     description='The most lit ros testing framework',
     long_description=long_description,
     packages=['pyrostest'],
