4e17a39591f75ed38e712663f417ed7fcbaced93
Joshua Morton <joshua.morton13@gmail.com>
2017-06-02 01:29:41 -0400

    Use relative path.

--- test.sh
+++ test.sh
@@ -18,7 +18,7 @@ sudo apt-get install -y python-catkin-pkg python-rosdep ros-indigo-catkin ros-in
 # Install our project globally
 cd ~/$CIRCLE_PROJECT_REPONAME
 python setup.py bdist_wheel
-pip install ~/dist/pyrostest-*
+pip install dist/pyrostest-*
 cd ~
 
 # Copy the rospackage to the catkin workspace
