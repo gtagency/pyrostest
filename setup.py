a7a8b2eb2f80f2bab436b2a608f3469911943cbf
Joshua Morton <joshua.morton13@gmail.com>
2017-06-03 18:35:19 -0400

    Increment version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test = [
 
 setup(
     name='pyrostest',
-    version='0.1.6',
+    version='0.1.8',
     description='The most lit ros testing framework',
     long_description=long_description,
     packages=['pyrostest'],
